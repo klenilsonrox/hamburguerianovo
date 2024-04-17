9dd2cbd6aa7a85570a3d064cff759f6eacd42bb9
klenilsonrox <klenilsoncoc@gmail.com>
2024-04-17 10:34:46 -0300

    att

--- src/app/components/Produto.js
+++ src/app/components/Produto.js
@@ -51,14 +51,14 @@ const Produto = ({item}) => {
     
  </div>
  {modalItem && <div className="fixed inset-0 flex bg-black bg-opacity-10 backdrop-blur-sm justify-center z-30">
-      <div className='w-full bg-white h-screen overflow-y-scroll max-w-md relative rounded-md '>
+      <div className='w-full bg-white h-screen overflow-y-scroll max-w-md relative rounded-md flex flex-col'>
 
         <div className='relative'>
             <img src={item.urlImage} alt={`imagem do ${item.produto}`} className='w-full'/>
             <button className='absolute top-2 right-2 bg-red-600 rounded-full w-8 h-8 text-white' id='btnfechar' onClick={()=>setModalItem(false)}>X</button>
         </div>
 
-        <div className='p-2 '>
+        <div className='p-2 flex-1'>
             <div>
             <h1 className='text-xl font-bold'>{item.produto}</h1>
             <p className='mt-2 font-medium'>R$ {Number(item.preco).toFixed(2)}</p>
@@ -66,10 +66,10 @@ const Produto = ({item}) => {
             </div>
             {item.categoria!=="bebidas" && item.categoria!=="bomboniere" && <div className='py-4'>
                 <p className='font-medium'>Alguma observação sobre o seu pedido?</p>
-                <textarea name="" id="" cols="30" rows="2" className='bg-slate-200 w-full mt-2 rounded-md p-2' placeholder='ex: sem milho, sem salada' value={observacao} onChange={({target})=>setObservacao(target.value)}></textarea>
+                <textarea name="" id="" cols="30" rows="3" className='bg-slate-200 w-full mt-2 rounded-md p-2' placeholder='ex: sem milho, sem salada' value={observacao} onChange={({target})=>setObservacao(target.value)}></textarea>
             </div>}
         </div>
-        <div className='relative right-2 flex items-center justify-end mb-2'>
+        <div className='relative right-2 flex items-center justify-end mb-2 bottom-2'>
            <button className='bg-red-600 text-white rounded-full py-2 px-6 font-medium' onClick={addItemAoCarrinho}>Adicionar ao carrinho</button>
         </div>
       </div>
