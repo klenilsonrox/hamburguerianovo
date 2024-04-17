068f48eb534893fda1de7dc3d410e534dad6e22e
klenilsonrox <klenilsoncoc@gmail.com>
2024-04-17 01:10:43 -0300

    at

--- src/app/components/Produto.js
+++ src/app/components/Produto.js
@@ -41,7 +41,7 @@ const Produto = ({item}) => {
    
   return (
     <>
-    <div key={item.id} className=' py-2 flex gap-2 cursor-pointer items-center  md:items-start border-b' onClick={()=>openModalItem(item)}>
+    <div key={item.id} className=' py-2 flex gap-2 cursor-pointer items-center justify-between  md:items-start border-b' onClick={()=>openModalItem(item)}>
      <img src={item.urlImage} alt={`imagem do ${item.produto}`} className='max-w-[90px] max-h-[90px] lg:max-w-[110px] lg:max-h-[110px]  rounded-md order-2 lg:order-none'/>
      <div className='flex flex-col justify-between gap-2'>
         <h1>{item.produto}</h1>
@@ -51,12 +51,12 @@ const Produto = ({item}) => {
     
  </div>
  {modalItem && <div className="fixed inset-0 flex bg-black bg-opacity-10 backdrop-blur-sm justify-center z-30">
-      <div className='w-full bg-white max-w-md relative mt-2 rounded-md overflow-hidden '>
+      <div className='w-full bg-white max-w-md relative rounded-md min-h-screen overflow-y-scroll '>
         <div className='relative'>
             <img src={item.urlImage} alt={`imagem do ${item.produto}`} className='w-full'/>
             <button className='absolute top-2 right-2 bg-red-600 rounded-full w-8 h-8 text-white' id='btnfechar' onClick={()=>setModalItem(false)}>X</button>
         </div>
-        <div className='p-2 max-h-[200px] overflow-y-scroll'>
+        <div className='p-2 '>
             <div>
             <h1 className='text-xl font-bold'>{item.produto}</h1>
             <p className='mt-2 font-medium'>R$ {Number(item.preco).toFixed(2)}</p>
@@ -64,10 +64,10 @@ const Produto = ({item}) => {
             </div>
             {item.categoria!=="bebidas" && item.categoria!=="bomboniere" && <div className='py-4'>
                 <p className='font-medium'>Alguma observação sobre o seu pedido?</p>
-                <textarea name="" id="" cols="30" rows="3" className='bg-slate-200 w-full mt-2 rounded-md p-2' placeholder='ex: sem milho, sem salada' value={observacao} onChange={({target})=>setObservacao(target.value)}></textarea>
+                <textarea name="" id="" cols="30" rows="2" className='bg-slate-200 w-full mt-2 rounded-md p-2' placeholder='ex: sem milho, sem salada' value={observacao} onChange={({target})=>setObservacao(target.value)}></textarea>
             </div>}
         </div>
-        <div className='absolute bottom-4 right-4'>
+        <div className='absolute bottom-4 right-4 '>
            <button className='bg-red-600 text-white rounded-full py-2 px-6 font-medium' onClick={addItemAoCarrinho}>Adicionar ao carrinho</button>
         </div>
       </div>
